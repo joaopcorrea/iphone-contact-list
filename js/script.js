f465312fd0af16f654ffcef44fb1f4a0af49d00b
João Pedro Corrêa <joaopedrocorrea21@gmail.com>
2022-11-23 22:30:51 -0300

    ✨ finishing keyboard

--- js/script.js
+++ js/script.js
@@ -24,8 +24,14 @@ window.addEventListener('click', (event) => {
   }
   else if (keyboard.contains(event.target)) {
     if (event.target.tagName === 'LI') {
-      document.getElementById(cur_id).value += event.target.innerText;
       document.getElementById(cur_id).focus();
+
+      handleKeyClick(event.target);
+    }
+    else if (event.target.parentNode.tagName === 'LI') {
+      document.getElementById(cur_id).focus();
+
+      handleKeyClick(event.target.parentNode);
     }
   }
   else {
@@ -34,6 +40,48 @@ window.addEventListener('click', (event) => {
     console.log(event.target.querySelector('.keyboard'));
   }
 })
+
+function handleKeyClick(li) {
+  let classes = li.classList;
+  if (classes.contains('shift')) {
+    if (keyboard.classList.contains('next-key-uppercase')) {
+      keyboard.classList.remove('next-key-uppercase');
+      keyboard.classList.add('full-uppercase');
+    }
+    else if (keyboard.classList.contains('full-uppercase')) {
+      keyboard.classList.remove('full-uppercase');
+    }
+    else {
+      keyboard.classList.add('next-key-uppercase');
+    }
+  }
+  else if (classes.contains('back')) {
+    document.getElementById(cur_id).value =
+      document.getElementById(cur_id).value.slice(0, -1);
+  }
+  else if (classes.contains('alt')) {
+    keyboard.querySelector('.letters').classList.toggle('hide');
+    keyboard.querySelector('.numbers').classList.toggle('hide');
+  }
+  else {
+    switch (li.innerText) {
+      case 'space':
+        document.getElementById(cur_id).value += ' ';
+        break;
+
+      case 'return':
+        document.getElementById(cur_id).value += '\n';
+        break;
+
+      default:
+        document.getElementById(cur_id).value += li.innerText;
+
+        if (keyboard.classList.contains('next-key-uppercase'))
+          keyboard.classList.remove('next-key-uppercase');
+        break;
+    }
+  }
+}
 
 function setId(id) {
   cur_id = id;
